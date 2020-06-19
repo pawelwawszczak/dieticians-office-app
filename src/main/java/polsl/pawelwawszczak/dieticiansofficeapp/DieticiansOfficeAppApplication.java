3ecefa8572d0b5661b43c9a0ce7d5ee61d522da9
pawelwawszczak <pawel.wawszczak1@gmail.com>
2020-06-19 16:31:18 +0200

    Initial project add

---
+++ src/main/java/polsl/pawelwawszczak/dieticiansofficeapp/DieticiansOfficeAppApplication.java
@@ -0,0 +1,13 @@
+package polsl.pawelwawszczak.dieticiansofficeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieticiansOfficeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DieticiansOfficeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/polsl/pawelwawszczak/dieticiansofficeapp/DieticiansOfficeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package polsl.pawelwawszczak.dieticiansofficeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieticiansOfficeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
