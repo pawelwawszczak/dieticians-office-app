a77f783f79a4e60e87afd79cb188b5a1508b9b4e
pawelwawszczak <44114727+pawelwawszczak@users.noreply.github.com>
2021-05-17 22:16:33 +0200

    Delete src/main/java/polsl/pawelwawszczak/dieticiansofficeapp/services directory
    Old package, not being implemented.

--- src/main/java/polsl/pawelwawszczak/dieticiansofficeapp/services/PatientService.java
+++
@@ -1,9 +0,0 @@
-package polsl.pawelwawszczak.dieticiansofficeapp.services;
-
-public interface PatientService {
-
-    Patient findByLastName(String lastName);
-    Patient findById(Long id);
-    Patient save(Patient patient);
-    Set<Patient> findAll();
-}
--- src/main/java/polsl/pawelwawszczak/dieticiansofficeapp/services/VisitService.java
+++
@@ -1,8 +0,0 @@
-package polsl.pawelwawszczak.dieticiansofficeapp.services;
-
-public interface VisitService {
-
-    Patient findById(Long id);
-    Patient save(Patient patient);
-    Set<Patient> findAll();
-}
