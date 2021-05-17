7eb6795cdcb21fb91b791178c6ef8998efdd08b8
pawelwawszczak <pawel.wawszczak1@gmail.com>
2021-05-17 22:09:45 +0200

    Adding security configuration

--- src/main/java/polsl/pawelwawszczak/dieticiansofficeapp/configuration/SecurityConfiguration.java
+++ src/main/java/polsl/pawelwawszczak/dieticiansofficeapp/configuration/SecurityConfiguration.java
@@ -48,7 +48,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .antMatchers("/dietician/**").hasRole("DIETICIAN")
